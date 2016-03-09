d735881fd9c2b53586dcf88a503673030ffc928e
Hans Maree <hans.maree@gmail.com>
2016-03-09 15:44:21 +0100

    Support for NES scoreboars.

--- import_scoreboard.py
+++ import_scoreboard.py
@@ -6,9 +6,9 @@ import sys
 
 regex_tgm = r'''
     ^[. ]*(\d+)         # Rank
-    [- ]+
-    ([^-]+)             # Name (assumes no '-' in name)
-    [- ]+
+    --
+    (.+)                # Name
+    ---*[ ]*
     ([^- ]+)            # Grade
     [- ]+
     ([\d-]*)            # Level
@@ -25,7 +25,7 @@ regex_tgm = r'''
     '''
 
 regex_death = r'''
-    ^(.*)               # Name
+    ^(.+)               # Name
     [ ]---*[ ]+
     (\d+)               # Level
     [@ ]+
@@ -38,7 +38,7 @@ regex_death = r'''
 
 
 regex_texmaster = r'''
-    ^(.*)               # Name
+    ^(.+)               # Name
     \ ---*[ ]+
     ([\dSMVKG]+(?:[ ]o)?)      # Class
     [ ]+
@@ -53,7 +53,7 @@ regex_texmaster = r'''
     '''
 
 regex_texmaster2 = r'''
-    ^(.*)               # Name
+    ^(.+)               # Name
     \ ---*[ ]+
     ([\dx]+)               # Level
     [ ]+
@@ -65,6 +65,32 @@ regex_texmaster2 = r'''
     )?
     '''
 
+regex_NES = r'''
+    ^[. ]*(\d+)         # Rank
+    [- ]+
+    (.+)                # Name
+    [- ]+
+    ([\d,]+)            # Score
+    [- ]+
+    (\d+[+][\d.]+)      # Level
+    [ -]
+    (.*)                # Comment
+    '''
+
+regex_NES_level19 = r'''
+    ^[. ]*(\d+)         # Rank
+    --
+    (.+)                # Name
+    ---*[ ]*
+    ([\d,]+)            # Score
+    [- ]+
+    ([\d?]+)            # Lines
+    [ -]+
+    ([\d?]+)            # pts/line
+    [ -]+
+    (.*)                # Comment
+    '''
+
 regex_table = {
         'TGM1': regex_tgm,
         'TAP_master': regex_tgm,
@@ -73,6 +99,8 @@ regex_table = {
         'texmaster_special': regex_texmaster,
         'texmaster_sudden_ti': regex_texmaster,
         'texmaster_sudden': regex_texmaster2,
+        'NES_NTSC_A_type': regex_NES,
+        'NES_NTSC_A_type_level19': regex_NES_level19,
         }
 
 
